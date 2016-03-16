c784e004025e2c0d6b27cc05cb68bd7bb72cc215
rMBP13 <numlock178@gmail.com>
2016-03-16 20:15:34 +0800

    viewer jiexi

--- spring-web/src/main/java/com/netease/course/web/controller/HelloController.java
+++ spring-web/src/main/java/com/netease/course/web/controller/HelloController.java
@@ -1,7 +1,11 @@
 package com.netease.course.web.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -28,9 +32,11 @@ public class HelloController {
     }
 
     @RequestMapping("/users/login")
-    @ResponseBody
-    public String login(@RequestParam("name") String name, @RequestParam("password") String password)
-            throws IOException {
-        return "Name: " + name + "<p />Password: " + password;
+    public String login(@RequestParam("name") String name,
+                        @RequestParam("password") String password,
+                        ModelMap map) throws IOException {
+        map.addAttribute("name", name);
+        map.addAttribute("password", password);
+        return "user";
     }
 }

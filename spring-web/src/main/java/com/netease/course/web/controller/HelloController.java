d04ecb3b9ca5a264e10f907c362f53cc68b091ad
rMBP13 <numlock178@gmail.com>
2016-03-16 17:45:11 +0800

    shixian  Controller

--- spring-web/src/main/java/com/netease/course/web/controller/HelloController.java
+++ spring-web/src/main/java/com/netease/course/web/controller/HelloController.java
@@ -1,21 +1,36 @@
 package com.netease.course.web.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.Writer;
 
-/**
- * Created by Eric on 2/19/16.
- */
 
 @Controller
 @RequestMapping(value = "/hello")
 public class HelloController {
+
     @RequestMapping(value = "/spring")
     public void spring(HttpServletResponse response) throws IOException {
-
         response.getWriter().write("Hello, Spring Web!!");
+    }
+
+    //http://localhost:8080/api/hello/users/123?msg=Hello
+    @RequestMapping("/users/{userId}")
+    public void writer(@PathVariable("userId") String userId,
+                       @RequestParam("msg") String msg,
+                       @RequestHeader("host") String host,
+                       Writer writer) throws IOException {
+        writer.write("Hello, here use Writer\n");
+        writer.write("userId=" + userId + ", msg=" + msg + ", hostHeader=" + host);
+    }
+
+    @RequestMapping("/users/login")
+    @ResponseBody
+    public String login(@RequestParam("name") String name, @RequestParam("password") String password)
+            throws IOException {
+        return "Name: " + name + "<p />Password: " + password;
     }
 }

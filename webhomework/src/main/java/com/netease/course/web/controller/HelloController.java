f166feb0f6da83ea09f592f2eef52e69cbe91463
rMBP13 <numlock178@gmail.com>
2016-03-17 14:05:49 +0800

    chapter5

--- webhomework/src/main/java/com/netease/course/web/controller/HelloController.java
+++ webhomework/src/main/java/com/netease/course/web/controller/HelloController.java
@@ -5,13 +5,15 @@ import freemarker.template.Template;
 import freemarker.template.TemplateExceptionHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
+import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -31,23 +33,36 @@ public class HelloController {
     }
 
 
-    @RequestMapping("/users/list")
-    public void list(Writer writer) throws Exception {
-        Configuration cfg = new Configuration(Configuration.VERSION_2_3_22);
-        cfg.setDirectoryForTemplateLoading(
-                new File("/Users/Eric/Documents/IdeaProjects/HeadFirst-Spring-Develop/webhomework/src/main/resources"));
-        cfg.setDefaultEncoding("UTF-8");
-        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+    @RequestMapping("/users/{path:.+}")
+    public void list(@PathVariable("path") String path,
+                     @RequestHeader("Accept") String accept,
+                     HttpServletResponse response) throws Exception {
+        System.out.println(path);
+        System.out.println(accept);
+        boolean isJson = accept.contains("application/json");
+        boolean isText = accept.contains("application/xml");
 
-        Template ftl = cfg.getTemplate("list.ftl");
+        PrintWriter writer = response.getWriter();
+        if (path.endsWith("json") || isJson) {
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            writer.write("{\"userList\":[{\"userId\": 1001,\"userName\":\"test1\"}," +
+                    "{\"userId\": 1002,\"userName\": \"test2\"}]}");
 
-        Map<String, Object> root = new HashMap<String, Object>();
-        root.put("userId1", "1001");
-        root.put("userName1", "test1");
-        root.put("userId2", "1002");
-        root.put("userName2", "test2");
+        } else {
+            Configuration cfg = new Configuration(Configuration.VERSION_2_3_22);
+            String file = "/Users/Eric/Documents/IdeaProjects/HeadFirst-Spring-Develop/webhomework/src/main/resources";
+            cfg.setDirectoryForTemplateLoading(new File(file));
+            cfg.setDefaultEncoding("UTF-8");
+            cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+            Template ftl = cfg.getTemplate("list.ftl");
 
-        Writer out = new OutputStreamWriter(System.out);
-        ftl.process(root, writer);
+            Map<String, Object> root = new HashMap<String, Object>();
+            root.put("userId1", "1001");
+            root.put("userName1", "test1");
+            root.put("userId2", "1002");
+            root.put("userName2", "test2");
+            ftl.process(root, writer);
+        }
     }
 }

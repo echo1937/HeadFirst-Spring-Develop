cb23ac0dd3d1a1f771ee293775c1bf1088377960
rMBP13 <numlock178@gmail.com>
2016-03-18 01:09:39 +0800

    考试第二题

---
+++ Spring-Login/src/main/java/com/netease/exam/dao/userDAO.java
@@ -0,0 +1,31 @@
+package com.netease.exam.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+
+
+@Repository
+public class UserDAO {
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public String selectUser(String userName) {
+        String pw = null;
+        try {
+            pw = (String) this.jdbcTemplate.queryForObject("SELECT userPassword FROM User WHERE UserName=?",
+                    new Object[]{userName}, String.class);
+        } catch (Exception e) {
+            return "";
+        }
+        return pw;
+    }
+
+}
---
+++ Spring-Login/src/main/webapp/WEB-INF/freemarker/failed.ftl
@@ -0,0 +1,10 @@
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <title>用户信息</title>
+</head>
+<body>
+<h2>Login Failed</h2>
+
+</body>
+</html>
